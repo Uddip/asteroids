4e9b972cb86d433bf84e7d7608bd74c868313a7f
Uddip Amin <uddip.amin@gmail.com>
2025-01-08 00:46:43 -0500

    Added player movement and groups

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from constants import *
 from player import *
 
 def main():
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
@@ -19,12 +21,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
+        for item in updateable:
+            item.update(dt)
+
         screen.fill("black")
 
-        player.draw(screen)
-        
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
