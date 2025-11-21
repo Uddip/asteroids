9333350bf8a91a1f413cd23cde7fe96eae74814f
Uddip Amin <uddip.amin@gmail.com>
2025-11-20 22:15:53 -0500

    Added logging and new opening message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
 import pygame
+import sys
 
 from constants import *
 from player import Player
@@ -8,6 +8,9 @@ from asteroidfield import AsteroidField
 from shot import Shot
 
 def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
